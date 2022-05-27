184f2da8d0b0be5d0fbb581fb07647b49cab0255
anurupkoli <anurupkoli01@gmail.com>
2022-05-27 11:25:43 +0530

    LITTLE

--- dice_rolling.py
+++ dice_rolling.py
@@ -1,5 +1,4 @@
 import random
-from sunau import AUDIO_FILE_MAGIC
 
 player_score = 0
 comp_score = 0
@@ -7,37 +6,39 @@ comp_score = 0
 print("Welcome my Friend, you have to score 50 points before computer does it to WIN!!")
 
 while True:
-    #Computer :
-    print("computer's turn: ")
-    comp = random.randint(1, 6)
-    print(f"Computer's Dice = {comp}")
-    comp_score += comp
-    print(f"Computer's score = {comp_score}")
     
     if comp_score >= 50 or player_score >= 50:
         break
 
-    #Player :
     dice1 = (input("Do you want to roll the dice?...click '(y/n)': "))
     dice = dice1.lower()
     if dice == "y":
+        #Player :
         dice_no = random.randint(1, 6)
         print("Your turn: ")
         print(f"Your Dice = {dice_no}")
         player_score += dice_no
         print(f"Your Score = {player_score}")
-        AUDIO_FILE_MAGIC
+        #Computer :
+        print("computer's turn: ")
+        comp = random.randint(1, 6)
+        print(f"Computer's Dice = {comp}")
+        comp_score += comp
+        print(f"Computer's score = {comp_score}")
+    
     elif dice == "n":
         break
     elif dice == "":
         print("Please enter valid alphabet '(y/n)'")
+        False
     else:
         print("Please enter valid alphabet '(y/n)'")
+        False
+  
+if comp_score >= 50:
+    print("Computer won!! Better  luck next time :)")
 
-if comp_score == 50:
-    print("Computer won!!")
-
-elif player_score == 50:
-    print("You won!!")
+elif player_score >= 50:
+    print("Congratulations.You won!!")
 
 print("Thankyou for playing :)")
